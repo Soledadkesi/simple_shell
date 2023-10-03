620a9543c392b0094462e31c168abce757ac2aaf
Soledadkesi <soledadkesi1995@gmail.com>
2023-10-03 01:01:07 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <error.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+
+
+
+#endif /* end of header*/
