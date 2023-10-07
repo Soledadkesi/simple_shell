f787ac96de5a9dfde7c7c8ad016c581f3379f93c
iDamina <daminailiya@gmail.com>
2023-10-06 23:26:27 -0700

    tokenizeOR.c

--- tokenizeOR.c
+++ tokenizeOR.c
@@ -1,7 +1,13 @@
 #include  "main.h"
+/**
+ * tokenizeOR - tokenize OR || operator
+ * @command: command
+ * Return: return command
+ */
 char *tokenizeOR(char *command)
 {
 	char *token = NULL;
+
 	token = strtok(command, "|\n");
 	return (token);
 }
